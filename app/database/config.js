c29e961393ba930e705c9a3530c0affc09ac2ad2
Gary Yaral <gary.yaral@gmail.com>
2024-04-03 10:09:41 -0500

    dialect

--- app/database/config.js
+++ app/database/config.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DB_URI)
+const sequelize = new Sequelize(process.env.DB_URI, {
+  dialect: 'postgres'
+})
 
 sequelize.authenticate()
   .then(() =>{
