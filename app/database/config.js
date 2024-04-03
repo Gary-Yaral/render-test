fd5213baa5b237373d9f3fb762a98244786a5c02
Gary Yaral <gary.yaral@gmail.com>
2024-04-03 10:28:26 -0500

    añadido ssl

--- app/database/config.js
+++ app/database/config.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DB_URI, {
-  dialect: 'postgres'
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true, // Requerir una conexión SSL/TLS
+      rejectUnauthorized: false // Desactivar la verificación del certificado (puedes establecerlo en 'true' en producción)
+    }
+  }
 })
 
 sequelize.authenticate()
